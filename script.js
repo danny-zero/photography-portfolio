04a0319ec1354bae4b475df1d849c0b936866c76
danny-zero <sifuarrow@gmail.com>
2021-07-21 23:23:40 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,84 @@
+//responsive navbar
+const toggleButton = document.querySelectorAll('.toggle-button')[0]
+const navbarLinks = document.querySelectorAll('.navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
+
+const navList = document.querySelectorAll('.nav-list');
+
+navList.forEach((ul) => {
+    ul.innerHTML = `
+    <li class="nav-link" value="japan"><a href="/pages/japan.html">japan</a></li>
+    <li class="nav-link" value="nyc"><a href="/pages/nyc.html">New York</a></li>
+    <li class="nav-link" value="vegas"><a href="/pages/vegas.html">vegas</a></li>
+    <li class="nav-link" value="archive"><a href="/pages/archive.html">archive</a></li>
+
+    <li class='nav-link'><a href="https://instagram.com/danny_l_photography" target="_blank" rel="noopener noreferrer">Instagram</a></li>
+    <li class='nav-link'><a href="https://www.etsy.com/shop/DannyLPhotoandDesign" target="_blank" rel="noopener noreferrer">Shop</a></li>
+    `
+});
+
+const navLinks = document.querySelectorAll('.nav-link');
+
+navLinks.forEach((link) => {
+    const value = link.getAttribute('value')
+    console.log(value)
+    const location = window.location.href
+    if (location.includes(value)) {
+        link.classList.add('underline')
+    }
+})
+
+const modal = document.querySelector('.customModal');
+let modalPhoto;
+
+if (modal) {
+    modalPhoto = document.querySelector('.modal-photo')
+    
+    let open = true;
+    
+    modal.addEventListener('click', () => {
+        if (open) {
+            modal.classList.remove('open')
+        }
+    })
+
+    const pics = document.querySelectorAll('.img-thumb')
+
+    pics.forEach((img) => {
+        img.addEventListener('click', () => {
+            modal.classList.add('open')
+        })
+    })
+}
+
+const openModal = () => {
+        console.log(event.target.src)
+        const imgUrl = event.target.src
+        modalPhoto.src = imgUrl
+}
+
+
+const carousel = document.querySelector('.carousel-img');
+const interior = document.querySelector('.home-images-interior');
+
+const images = [];
+
+// let num = 1;
+
+setInterval(() => {
+    // if (num === 15) { //amount of files in carousel folder
+    //     num = 1
+    // } else {
+    //     num++
+    // }
+    const num = Math.floor(Math.random() * (16-1) + 1)
+    // carousel.classList.remove('animate__fadeIn');
+    // carousel.classList.add('animate__fadeIn');
+    carousel.src = `assets/Carousel/carousel${num}.jpg`;
+    // interior.innerHTML = `<img class="carousel-img animate__animated animate__fadeIn" src="${images[num]}">`
+}, 4000)
+
---
+++ server/index.js
@@ -0,0 +1,65 @@
+//getting images from folder
+const axios = require('axios');
+const express = require('express');
+const { static } = express;
+const path = require('path');
+const fs = require('fs');
+const app = express();
+
+app.get('/folder/:folderName', (req, res, next) => {
+    try {
+        const folderName = req.params.folderName
+        const imagesArr=[];
+        let str = ''
+        const images = path.join(__dirname, `../assets/photos/${folderName}`)
+        console.log(images)
+        fs.readdir(images, (err, files) => {
+            try {
+               files.forEach((file) => {
+                   if (file !== '.DS_Store') {
+                    const imgTag = `&lt;img onclick='openModal()' class='img-thumb' src='/assets/photos/${folderName}/${file}' /&gt;`
+                    console.log(imgTag)
+                    imagesArr.push(imgTag)
+                    str += `${imgTag}</br>`
+                   }
+               }) 
+               res.send(str)
+            } catch (error) {
+                console.log('files error', error)
+            }
+        })
+    } catch (error) {
+        console.error(error)
+    }
+});
+
+app.get('/nav', (req, res, next) => {
+    try {
+        let str = ''
+        const pages = path.join(__dirname, '../pages');
+        console.log(pages)
+        fs.readdir(pages, (err, files) => {
+            try {
+                files.forEach((link) => {
+                    //<li class='nav-link' value='japan'><a href="/pages/japan.html">Japan</a></li>
+                    console.log(link)
+                    let name = link.split('.')[0]
+                    if (name === 'nyc') name = 'New York'
+                    const li = `&lt;li class="nav-link" value="${name}"&gt;&lt;a href="/pages/${link}"&gt;${name}&lt;/a&gt;&lt;/li&gt;`
+                    str += `${li}<br/>`
+                })
+                str += `&lt;li class='nav-link'&gt;&lt;a href="https://instagram.com/danny_l_photography" target="_blank" rel="noopener noreferrer"&gt;Instagram&lt;/a&gt;&lt;/li&gt;<br/>`
+                str += `&lt;li class='nav-link'&gt;&lt;a href="https://www.etsy.com/shop/DannyLPhotoandDesign" target="_blank" rel="noopener noreferrer"&gt;Shop&lt;/a&gt;&lt;/li&gt;<br/>`
+                res.send(str)
+            } catch (error) {
+                console.log('nav readdir error', error)
+            }
+        })
+    } catch (error) {
+        console.error('nav error', error)
+    }
+})
+
+app.listen(3000, () => {
+    console.log('listening on port 3000')
+})
